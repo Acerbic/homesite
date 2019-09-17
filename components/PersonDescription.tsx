354abf16978b4e0ceff7f59d9d168ce38f6d4fdc
Anthony Cerbic <kuzzdra@gmail.com>
2019-09-18 00:33:15 +0300

    PesonDescription component

--- components/PersonDescription.tsx
+++ components/PersonDescription.tsx
@@ -1,29 +1,60 @@
 import React from "react";
+import { Typography, Card, List } from "antd";
+const { Paragraph, Text, Title } = Typography;
+import css from "styled-jsx/css";
+
+const { className, styles } = css.resolve`
+    * :global(div.ant-typography),
+    * :global(span.ant-typography) {
+        font-size: 1.2em;
+    }
+    * :global(h1.ant-typography) {
+        text-align: center;
+    }
+`;
+
 export default () => (
-    <>
-        <p>
+    <Card className={className}>
+        <style jsx>{`
+            dl {
+                display: grid;
+                grid-template-columns: 30% 70%;
+            }
+        `}</style>
+        {styles}
+        <Title>About me</Title>
+        <Paragraph>
             A highly self-motivated Web Developer with over 10 years of
             experience and academic background. Main languages are
             Javascript/Typescript and PHP. Possesses theoretical and practical
-            knowledge of programming patterns, algorithms and data structures,
+            knowledge of programming, patterns, algorithms and data structures,
             adheres to the best industry practices. Fluent in Russian and
             English , can speak beginner French and German.
-        </p>
-        <p>
-            Core competencies include: <br />
-            <br />
-            Languages: Javascript(ES6), Typescript , PHP, SQL, HTML, CSS,
-            GraphQL
-            <br />
-            Libraries and frameworks: NextJS, ReactJS, Laravel/Lumen, VueJS,
-            jQuery, Twitter Bootstrap, Ant.Design, XState, websocket.io, Prisma
-            <br />
-            Technologies: Algorithms and data structures, Test-driven
-            development, Client-server architecture, Relational databases, MVC
-            patterns, RESTful APIs, Parallelism in execution
-            <br />
-            Tooling: Git, NodeJs, npm, yarn, Docker, composer, webpack, babel 7,
-            VS Code
-        </p>
-    </>
+        </Paragraph>
+        <Paragraph>
+            <Title level={4}>Core competencies include:</Title>
+            <dl>
+                <dt>Languages</dt>
+                <dd>
+                    Javascript(ES6), Typescript , PHP, SQL, HTML, CSS, GraphQL
+                </dd>
+                <dt>Libraries and frameworks</dt>
+                <dd>
+                    NextJS, ReactJS, Laravel/Lumen, VueJS, jQuery, Twitter
+                    Bootstrap, Ant.Design, XState, websocket.io, Prisma
+                </dd>
+                <dt>Technologies</dt>
+                <dd>
+                    Algorithms and data structures, Test-driven development,
+                    Client-server architecture, Relational databases, MVC
+                    patterns, RESTful APIs, Parallelism in execution
+                </dd>
+                <dt>Tooling</dt>
+                <dd>
+                    Git, NodeJs, npm, yarn, Docker, composer, webpack, babel 7,
+                    VS Code
+                </dd>
+            </dl>
+        </Paragraph>
+    </Card>
 );
